a1f86c020ed247a2bdd23d632b4342d73244f418
Nate Magera <nmagera07@gmail.com>
2019-07-29 13:39:18 -0400

    init

---
+++ mesofunny/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = (props) => {
+    return ( 
+        <div>
+            <h1>Public Dad Jokes</h1>
+        </div>
+     );
+}
+ 
+export default HomePage;
